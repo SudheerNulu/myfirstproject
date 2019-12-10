b67399410afa4108adb993ed50236d76bef225be
sudheer <sudheer.nulu@synechron.com>
2019-12-10 10:47:28 +0530

    Updating calc.py with logic

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    "Add Function"
+    return x+y
+
+
+def subtract(x,y):
+    """subtract function"""
+    return x-y
+
+
+def multiply(x,y):
+    """subtract function"""
+    return x*y
+
+
+def divide(x,y):
+    """subtract function"""
+    return x/y
+
